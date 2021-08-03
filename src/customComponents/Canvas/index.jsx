df0596bbdf7fdfe1f4e7782e5b1e16242db709ab
kityandhero <kityandhero@126.com>
2021-08-03 12:08:22 +0800

    update

--- src/customComponents/Canvas/index.jsx
+++
@@ -1,7 +0,0 @@
-import BubblySource from './Bubbly';
-import RadarScanningSource from './RadarScanning';
-import SpiritSource from './Spirit';
-
-export const Bubbly = BubblySource;
-export const RadarScanning = RadarScanningSource;
-export const Spirit = SpiritSource;
--- src/framework/DataModal/BaseUpdateModal/index.jsx
+++
@@ -1,5 +0,0 @@
-import BaseLoadModal from '../BaseLoadModal';
-
-class BaseUpdateModal extends BaseLoadModal {}
-
-export default BaseUpdateModal;
