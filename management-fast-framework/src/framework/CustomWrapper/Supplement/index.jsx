cace203be52a2f55e96ff7909383bce9a19e1294
kityandhero <kityandhero@126.com>
2021-08-03 17:09:41 +0800

    update

--- management-fast-framework/src/framework/CustomWrapper/Supplement/index.jsx
+++ management-fast-framework/src/framework/CustomWrapper/Supplement/index.jsx
@@ -1,16 +1,6 @@
-import {
-  getDerivedStateFromPropsForUrlParams,
-  refitCommonData,
-  isInvalid,
-  searchFromList,
-  isUndefined,
-  isNull,
-} from '../../../utils/tools';
-import { unlimitedWithStringFlag } from '../../../utils/constants';
+import { getDerivedStateFromPropsForUrlParams } from '../../../utils/tools';
 
 import SupplementCore from '../SupplementCore';
-
-const unknownLabel = '未知';
 
 /**
  * 该类作为特有项目的补充，视具体项目进行增部方法
