eed748b0a7c01408d80de0199a737cbe65da2114
kityandhero <kityandhero@126.com>
2021-08-04 11:47:15 +0800

    update

--- example/config/config.local.js
+++ example/config/config.local.js
@@ -6,11 +6,9 @@ import webpackPlugin from './plugin.config';
 
 const { winPath } = utils;
 
-const corsTargetDomain = 'http://s_m_api.b.com';
-const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, REACT_APP_ENV } =
-  process.env;
-const isAntDesignProPreview =
-  ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
+const corsTargetDomain = '';
+const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION, REACT_APP_ENV } = process.env;
+const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
 
 export default defineConfig({
   favicon: `${corsTargetDomain}/assists/image/favicon.ico`,
