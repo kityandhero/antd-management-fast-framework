b24f2e155026d3d0e85100ef0ae3d4943cf09e7d
kityandhero <kityandhero@126.com>
2021-08-18 23:24:55 +0800

    update

--- example/src/models/user.js
+++ example/src/models/user.js
@@ -1,5 +1,6 @@
 import { pretreatmentRemoteSingleData } from 'antd-management-fast-framework/lib/utils/requestAssistor';
-import { query as queryUsers, queryCurrent } from '../services/user';
+
+import { query as queryUsers, queryCurrent } from '@/services/user';
 
 export default {
   namespace: 'user',
