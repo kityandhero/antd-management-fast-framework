0061ddad24a81c9e860c9b39b127df9cff3441e8
kityandhero <kityandhero@126.com>
2021-08-18 21:05:21 +0800

    update

--- example/src/pages/Welcome.jsx
+++ example/src/pages/Welcome.jsx
@@ -9,11 +9,7 @@ export default () => (
       }}
     >
       Want to add more pages? Please refer to{' '}
-      <a
-        href="https://pro.ant.design/docs/block-cn"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
         use block
       </a>
       。
