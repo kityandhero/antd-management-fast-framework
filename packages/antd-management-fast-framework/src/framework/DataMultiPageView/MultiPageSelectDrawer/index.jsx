b2eb83dbe221a3beb8a2c8e585b1f0145df80901
卢志涛 <kityandhero@126.com>
2021-09-17 16:32:34 +0800

    feat: update
    affects: antd-management-fast-framework

---
+++ packages/antd-management-fast-framework/src/framework/DataMultiPageView/MultiPageSelectDrawer/index.jsx
@@ -0,0 +1,36 @@
+import MultiPageDrawer from '../MultiPageDrawer';
+
+class MultiPageSelectDrawer extends MultiPageDrawer {
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return super.getDerivedStateFromProps(nextProps, prevState);
+  }
+
+  selectRecord = ({ handleData }) => {
+    const { afterSelectSuccess } = this.props;
+
+    if (isFunction(afterSelectSuccess)) {
+      afterSelectSuccess(handleData);
+    }
+
+    this.hideDrawer();
+  };
+
+  renderSelectButton = ({
+    title: titleSource = '选择这条数据，确定吗？',
+    text: textSource = '选择',
+    icon: iconSource = <ImportOutlined />,
+    handleData = null,
+  }) => {
+    return buildButton({
+      size: 'small',
+      icon: iconSource || <ImportOutlined />,
+      text: textSource || '选择',
+      handleClick: () => this.selectRecord({ handleData: handleData || null }),
+      confirm: {
+        title: titleSource || '选择这条数据，确定吗？',
+      },
+    });
+  };
+}
+
+export default MultiPageSelectDrawer;
