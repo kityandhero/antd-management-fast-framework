3fd0da5c7836143313d8fb5f6520091fecbc8817
卢志涛 <kityandhero@126.com>
2021-10-26 18:42:29 +0800

    feat: update dependences
    affects: antd-management-fast-framework, simple-antd-management-fast-framework

--- packages/simple-antd-management-fast-framework/.stylelintrc.js
+++ packages/simple-antd-management-fast-framework/.stylelintrc.js
@@ -2,8 +2,10 @@ const fabric = require('@umijs/fabric');
 
 module.exports = {
   ...fabric.stylelint,
+  customSyntax: 'postcss-less',
   rules: {
     'unicode-bom': 'never',
     'no-descending-specificity': null,
+    'selector-class-pattern': null,
   },
 };
