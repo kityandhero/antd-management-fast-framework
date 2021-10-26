2496d9b93e08979f0696719de5488181b3ffec86
卢志涛 <kityandhero@126.com>
2021-10-26 19:49:17 +0800

    feat: update
    affects: antd-management-fast-framework, simple-antd-management-fast-framework

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+const fabric = require('@umijs/fabric');
+
+module.exports = {
+  ...fabric.stylelint,
+  customSyntax: 'postcss-less',
+  rules: {
+    'unicode-bom': 'never',
+    'no-descending-specificity': null,
+    'selector-class-pattern': null,
+  },
+};
