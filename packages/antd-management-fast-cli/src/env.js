facdbc35cb5a3a5073a7c559a6940880bd21e148
kityandhero <kityandhero@126.com>
2022-05-30 12:38:50 +0800

    style(antd-management-fast-cli): update

--- packages/antd-management-fast-cli/src/env.js
+++ packages/antd-management-fast-cli/src/env.js
@@ -4,8 +4,15 @@ const term = require("terminal-kit").terminal;
 const shell = require("shelljs");
 const download = require("download");
 const agent = require("hpagent");
+const ping = require("ping");
 
 const { HttpsProxyAgent } = agent;
+
+const packageUrlGithub =
+  "https://raw.githubusercontent.com/kityandhero/antd-management-fast-framework/master/packages/antd-management-fast-framework/package.json";
+
+const packageUrlGitee =
+  "https://gitee.com/lzt/antd-management-fast-framework/raw/master/packages/antd-management-fast-framework/package.json";
 
 exports.run = async function (s, o) {
   const {
@@ -14,11 +21,68 @@ exports.run = async function (s, o) {
 
   let packageTempPath = "";
 
-  if (agent) {
-    console.log(`agent: ${agent}`);
+  showMessage(`prepare to update package.json: `);
 
-    const packageUrl =
-      "https://raw.githubusercontent.com/kityandhero/antd-management-fast-framework/master/packages/antd-management-fast-framework/package.json";
+  showLine();
+
+  try {
+    packageTempPath = await handleTempPackagePath({
+      agent,
+      file,
+      packageUrl: packageUrlGithub,
+      repo: "github",
+    });
+  } catch (error) {
+    showLine();
+
+    showWarn(
+      `use github repo failure! switch to gitee, gitee repo possible update delay.`
+    );
+
+    showLine();
+
+    try {
+      packageTempPath = await handleTempPackagePath({
+        agent: "",
+        file,
+        packageUrl: packageUrlGitee,
+        repo: "gitee",
+      });
+    } catch (error) {
+      showLine();
+
+      console.error(error);
+
+      showLine();
+
+      showWarn("download error, please check network");
+
+      process.exit();
+    }
+  }
+
+  handlePackage(packageTempPath);
+};
+
+async function handleTempPackagePath({ agent, file, packageUrl, repo }) {
+  let packageTempPath = "";
+
+  if (file) {
+    showMessage(`use local referential package.json`);
+
+    showMessage(`file path: ${file}`);
+
+    showLine();
+
+    packageTempPath = resolve(file);
+  } else {
+    showMessage(`try ${repo} repo`);
+
+    if (agent) {
+      showMessage(`agent: ${agent}`);
+    }
+
+    showMessage(`${repo} repo: ${packageUrl}`);
 
     await download(packageUrl, resolve(`./temp`), {
       ...(agent
@@ -37,12 +101,18 @@ exports.run = async function (s, o) {
         : {}),
     });
 
-    term.green(`install develop environment by repo:main\r`);
+    showMessage(`use ${repo} repo success!`);
 
     packageTempPath = resolve(`./temp/package.json`);
-  } else {
-    packageTempPath = resolve(file);
+
+    showMessage(`packageTempPath: ${packageTempPath}`);
   }
+
+  return packageTempPath;
+}
+
+function handlePackage(packageTempPath) {
+  showMessage(`referential package.json :${packageTempPath}`);
 
   const packageProjectPath = resolve(`./package.json`);
 
@@ -58,7 +128,9 @@ exports.run = async function (s, o) {
 
           fs.writeJson(packageProjectPath, p)
             .then(() => {
-              term.green(`update package.json success!\r`);
+              showMessage(`update package.json success!`);
+
+              showLine();
 
               process.exit();
             })
@@ -79,4 +151,16 @@ exports.run = async function (s, o) {
 
       process.exit();
     });
-};
+}
+
+function showMessage(message) {
+  term.green(`${message}\r\n`);
+}
+
+function showWarn(message) {
+  term.red(`${message}\r\n`);
+}
+
+function showLine() {
+  term.gray("------------------------------------\r\n");
+}
