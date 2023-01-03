81cdfcfde0e13ea58080b43c4faa6613659b3558
卢志涛 <kityandhero@126.com>
2023-01-03 09:18:45 +0800

    feat(antd-management-fast-cli): update

--- packages/example-antd-management-fast-framework/src/pages/Home/index.jsx
+++ packages/example-antd-management-fast-framework/src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
 
+import { trim } from 'antd-management-fast-common/es/utils/tools';
+
 import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
 
 import styles from './index.less';
 
