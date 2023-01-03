3af45a7dd4f115899d3c70cbced4a49be6582bfc
卢志涛 <kityandhero@126.com>
2023-01-03 15:17:32 +0800

    feat(antd-management-fast-cli): update

--- packages/antd-management-fast-framework/src/configGroup/configGeneral.js
+++ packages/antd-management-fast-framework/src/configGroup/configGeneral.js
@@ -1,118 +1,95 @@
-import { defineConfig, utils } from 'umi';
+// import { defineConfig, utils } from 'umi';
 
-import { webpackPlugin5 } from './webpackPlugin';
+// import { webpackPlugin5 } from './webpackPlugin';
 
-const { REACT_APP_ENV } = process.env;
+// const { REACT_APP_ENV } = process.env;
 
-const { winPath } = utils;
+// const { winPath } = utils;
 
-export const configGeneral = {
+export const configDefault = {
+  mfsu: {
+    shared: {
+      react: {
+        singleton: true,
+      },
+    },
+  },
   hash: true,
-  alias: {},
-  antd: {
-    // dark: true,
-  },
-  analytics: false,
-  dva: {
-    hmr: true,
-  },
-  locale: {
-    default: 'zh-CN',
-    antd: true,
-    baseNavigator: false,
-  },
-  targets: {
-    ie: 11,
-  },
-  title: false,
-  theme: {},
-  define: {
-    REACT_APP_ENV: REACT_APP_ENV || false,
-  },
-  ignoreMomentLocale: true,
-  lessLoader: {
-    javascriptEnabled: true,
-  },
-  manifest: {
-    basePath: '/',
-  },
-  history: {
-    type: 'hash',
-  },
-  ssr: false,
-  mfsu: {},
-  fastRefresh: {},
-  extraCustomOption: {
-    babelCompact: false,
-  },
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {},
+  dva: {},
 };
 
-export function buildConfig({
-  routes,
-  loading = '@/components/PageLoading/index',
-  headerExtraLinks = [],
-  mfsu = true,
-  proxy = null,
-}) {
-  const c = {
-    ...configGeneral,
-    ...{
-      dynamicImport: {
-        loading,
-      },
-      routes,
-      cssLoader: {
-        modules: {
-          getLocalIdent: (context, _, localName) => {
-            if (
-              context.resourcePath.includes('node_modules') ||
-              context.resourcePath.includes('ant.design.pro.less') ||
-              context.resourcePath.includes('global.less')
-            ) {
-              return localName;
-            }
-            const match = context.resourcePath.match(/src(.*)/);
-            if (match && match[1]) {
-              const antdProPath = match[1].replace('.less', '');
-              const arr = winPath(antdProPath)
-                .split('/')
-                .map((a) => a.replace(/([A-Z])/g, '-$1'))
-                .map((a) => a.toLowerCase());
-              return `antd-pro${arr.join('-')}-${localName}`.replace(
-                /--/g,
-                '-',
-              );
-            }
-            return localName;
-          },
-        },
-      },
-    },
-    ...{
-      chainWebpack: webpackPlugin5,
-    },
-    ...((proxy || null) == null
-      ? {}
-      : {
-          proxy,
-        }),
-    ...(mfsu
-      ? {
-          mfsu: {},
-        }
-      : {
-          mfsu: false,
-        }),
-    ...{
-      extraCustomOption: {
-        headerExtraLinks: [...headerExtraLinks],
-        babelCompact: false,
-      },
-    },
-  };
+// export function buildConfig({
+//   routes,
+//   loading = '@/components/PageLoading/index',
+//   headerExtraLinks = [],
+//   mfsu = true,
+//   proxy = null,
+// }) {
+//   const c = {
+//     ...configGeneral,
+//     ...{
+//       dynamicImport: {
+//         loading,
+//       },
+//       routes,
+//       cssLoader: {
+//         modules: {
+//           getLocalIdent: (context, _, localName) => {
+//             if (
+//               context.resourcePath.includes('node_modules') ||
+//               context.resourcePath.includes('ant.design.pro.less') ||
+//               context.resourcePath.includes('global.less')
+//             ) {
+//               return localName;
+//             }
+//             const match = context.resourcePath.match(/src(.*)/);
+//             if (match && match[1]) {
+//               const antdProPath = match[1].replace('.less', '');
+//               const arr = winPath(antdProPath)
+//                 .split('/')
+//                 .map((a) => a.replace(/([A-Z])/g, '-$1'))
+//                 .map((a) => a.toLowerCase());
+//               return `antd-pro${arr.join('-')}-${localName}`.replace(
+//                 /--/g,
+//                 '-',
+//               );
+//             }
+//             return localName;
+//           },
+//         },
+//       },
+//     },
+//     ...{
+//       chainWebpack: webpackPlugin5,
+//     },
+//     ...((proxy || null) == null
+//       ? {}
+//       : {
+//           proxy,
+//         }),
+//     ...(mfsu
+//       ? {
+//           mfsu: {},
+//         }
+//       : {
+//           mfsu: false,
+//         }),
+//     ...{
+//       extraCustomOption: {
+//         headerExtraLinks: [...headerExtraLinks],
+//         babelCompact: false,
+//       },
+//     },
+//   };
 
-  return defineConfig(c);
-}
+//   return defineConfig(c);
+// }
 
 /**
  * 占位函数
