920813ad44cd46584232f12f55304b28254b1900
卢志涛 <kityandhero@126.com>
2023-03-22 10:42:40 +0800

    fix(antd-management-fast-charts): update

--- packages/antd-management-fast-config/config/rollupConfig.js
+++ packages/antd-management-fast-config/config/rollupConfig.js
@@ -10,7 +10,7 @@ export function buildConfig({ terser: whetherTerser = false }) {
     terser: whetherTerser,
     externalCollection: [],
     babelConfig: {
-      babelHelpers: 'inline',
+      // babelHelpers: 'inline',
     },
   });
 }
