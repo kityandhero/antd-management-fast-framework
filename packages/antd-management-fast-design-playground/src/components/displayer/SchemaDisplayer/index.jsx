b870cd647d46c4ccdf345051ca1203b29c06e6b7
卢志涛 <kityandhero@126.com>
2023-09-05 21:52:13 +0800

    fix(antd-management-fast-charts): update

---
+++ packages/antd-management-fast-design-playground/src/components/displayer/SchemaDisplayer/index.jsx
@@ -0,0 +1,87 @@
+import { Card, Rate, Slider } from 'antd';
+import React, { PureComponent } from 'react';
+import {
+  ArrayCards,
+  ArrayTable,
+  Cascader,
+  Checkbox,
+  DatePicker,
+  Editable,
+  Form,
+  FormCollapse,
+  FormGrid,
+  FormItem,
+  FormLayout,
+  FormTab,
+  Input,
+  NumberPicker,
+  Password,
+  PreviewText,
+  Radio,
+  Reset,
+  Select,
+  Space,
+  Submit,
+  Switch,
+  TimePicker,
+  Transfer,
+  TreeSelect,
+  Upload,
+} from '@formily/antd-v5';
+import { createForm } from '@formily/core';
+import { createSchemaField } from '@formily/react';
+
+const Text = ({ value, mode, content, ...properties }) => {
+  const tagName = mode === 'normal' || !mode ? 'div' : mode;
+  return React.createElement(tagName, properties, value || content);
+};
+
+const SchemaField = createSchemaField({
+  components: {
+    Space,
+    FormGrid,
+    FormLayout,
+    FormTab,
+    FormCollapse,
+    ArrayTable,
+    ArrayCards,
+    FormItem,
+    DatePicker,
+    Checkbox,
+    Cascader,
+    Editable,
+    Input,
+    Text,
+    NumberPicker,
+    Switch,
+    Password,
+    PreviewText,
+    Radio,
+    Reset,
+    Select,
+    Submit,
+    TimePicker,
+    Transfer,
+    TreeSelect,
+    Upload,
+    Card,
+    Slider,
+    Rate,
+  },
+});
+
+class SchemaDisplayer extends PureComponent {
+  render() {
+    const { form: formProperties, schema } = this.props;
+
+    const form = createForm();
+
+    return (
+      <Form {...formProperties} form={form}>
+        <SchemaField schema={schema} />
+      </Form>
+    );
+  }
+}
+
+export { SchemaDisplayer };
--- packages/antd-management-fast-design-playground/src/index.jsx
+++ packages/antd-management-fast-design-playground/src/index.jsx
@@ -2,6 +2,7 @@
 
 import './index.less';
 
+export * from './components/displayer';
 export * from './components/playground';
 export * from './components/playground/service';
 export * from './components/playground/widgets';
